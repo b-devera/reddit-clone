92b453afbd056e184d9a5207c27a45cb3c45716f
Berlun Devera <berlun_devera@icloud.com>
2022-07-10 23:01:36 -0700

    implemented backend
    implemented StepZen middleware and GraphQL API to connect to PostgreSQL database

---
+++ .history/components/Header_20220710010748.tsx
@@ -0,0 +1,128 @@
+import Image from "next/image";
+import React from "react";
+import {
+  MenuIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  SearchIcon,
+} from "@heroicons/react/solid";
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+function Header() {
+  const { data: session } = useSession();
+
+  return (
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
+      {/* Reddit Icon */}
+      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+        <Image
+          objectFit="contain"
+          src="https://links.papareact.com/fqy"
+          layout="fill"
+        />
+      </div>
+
+      {/* Home & Chevron Icons */}
+      <div className="mx-7 flex items-center xl:min-w-[300px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className="ml-2 hidden flex-1 lg:inline">Home</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
+
+      {/* Search Box */}
+      <form className="flex flex-1 items-center space-x-2 border border-gray-200 rounded-lg bg-gray-100 px-3 py-1">
+        <SearchIcon className="h-6 w-6 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Reddit"
+        />
+        <button type="submit" hidden />
+      </form>
+
+      {/* Icons */}
+      <div className="flex text-gray-500 space-x-2 items-center-500 mx-5 hidden lg:inline-flex">
+        <SparklesIcon className="icon" />
+        <GlobeIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="h-10 border border-gray-100" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <SpeakerphoneIcon className="icon" />
+      </div>
+
+      {/* Menu Icon */}
+      <div className="ml-5 flex items-center lg:hidden">
+        <MenuIcon className="icon" />
+      </div>
+
+      {/* Sign in & Sign out Buttons */}
+      {session ? (
+        <div
+          onClick={() => signOut()}
+          className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex"
+        >
+          <div className="relative h-6 w-5 flex-shrink-0 text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </div>
+
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session?.user?.name}</p>
+            <p className="text-gray-400">1 Karma</p>
+          </div>
+
+          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400" />
+        </div>
+      ) : (
+        <div
+          onClick={() => signIn()}
+          className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex"
+        >
+          <div className="relative h-6 w-5 flex-shrink-0 text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </div>
+
+          <p className="text-gray-400">Sign In</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Header;
--- .history/pages/api/auth/[...nextauth]_20220710003206.js
+++ .history/pages/api/auth/[...nextauth]_20220710003206.js
@@ -4,10 +4,10 @@ import RedditProvider from "next-auth/providers/reddit";
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    RedditProvider({
-        clientId: process.env.REDDIT_CLIENT_ID,
-        clientSecret: process.env.REDDIT_CLIENT_SECRET
-    }),
+        RedditProvider({
+          clientId: process.env.REDDIT_CLIENT_ID,
+          clientSecret: process.env.REDDIT_CLIENT_SECRET
+        })
     // ...add more providers here
   ],
 })
