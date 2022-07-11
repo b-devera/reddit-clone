8cdab0e202f77ac70a1c4844633b08eccb61cb38
Berlun Devera <berlun_devera@icloud.com>
2022-07-11 00:44:22 -0700

    added postbox ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,19 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import PostBox from "../components/PostBox";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-5xl my-7 mx-auto">
       <Head>
         <title>Reddit Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {/* PostBox */}
+      <PostBox />
+      <div>{/* Feed */}</div>
     </div>
   );
 };
